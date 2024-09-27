cc46856e16b2668d570f753cdd65cb0ff47259e0
antoniolg <antonio@devexperto.com>
2024-09-27 11:27:38 +0200

    Canvas: Draw Rect with drawBehind

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -1,9 +1,18 @@
 package io.devexpert.training.ui
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Playground() {
-    Text("Hello, World!")
+    Spacer(modifier = Modifier
+        .fillMaxSize()
+        .drawBehind {
+            drawRect(color = Color.Magenta, size = size / 2f)
+        }
+    )
 }

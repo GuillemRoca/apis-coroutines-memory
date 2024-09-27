98e2ec404a57051b522e49baaa2a82829c87aacc
antoniolg <antonio@devexperto.com>
2024-09-27 22:35:21 +0200

    LazyList: Custom arrangement

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -1,50 +1,60 @@
 package io.devexpert.training.ui
 
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalFoundationApi::class)
+object TopWithFooter : Arrangement.Vertical {
+    override fun Density.arrange(
+        totalSize: Int,
+        sizes: IntArray,
+        outPositions: IntArray
+    ) {
+        var y = 0
+        sizes.forEachIndexed { index, size ->
+            outPositions[index] = y
+            y += size
+        }
+        if (y < totalSize) {
+            val lastIndex = outPositions.lastIndex
+            outPositions[lastIndex] = totalSize - sizes.last()
+        }
+    }
+}
+
 @Composable
 fun Playground() {
     val listState = rememberLazyListState()
-    val items = (1..50).toList()
-    val groupedItems = items.groupBy { it / 10 }
+    val items = (1..5).toList()
 
-    LazyColumn(state = listState, modifier = Modifier.fillMaxSize()) {
-        groupedItems.forEach { (header, group) ->
-            stickyHeader {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    color = MaterialTheme.colorScheme.primary
-                ) {
-                    Text(
-                        text = "Header ${header * 10}",
-                        modifier = Modifier.padding(16.dp),
-                        style = MaterialTheme.typography.labelLarge
-                    )
-                }
-            }
-            items(group) { item ->
+    LazyColumn(
+        state = listState,
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = TopWithFooter
+    ) {
+
+        items(items) { item ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
                 Text(
                     text = "Item $item",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    style = MaterialTheme.typography.bodyLarge
+                    modifier = Modifier.padding(16.dp)
                 )
             }
         }
+
     }
 }

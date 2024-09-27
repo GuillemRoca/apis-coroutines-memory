ab6996dfc188b45e7647f62003c93214ec8dff45
antoniolg <antonio@devexperto.com>
2024-09-27 11:39:26 +0200

    Canvas: Use Canvas composable

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -1,18 +1,14 @@
 package io.devexpert.training.ui
 
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Playground() {
-    Spacer(modifier = Modifier
-        .fillMaxSize()
-        .drawBehind {
-            drawRect(color = Color.Magenta, size = size / 2f)
-        }
-    )
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        drawRect(color = Color.Magenta, size = size / 2f)
+    }
 }

ec66c36d3e1b8990180d42ccc15ddee1960a050a
antoniolg <antonio@devexperto.com>
2024-09-27 12:10:54 +0200

    Canvas: custom paths

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -4,14 +4,20 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
-import io.devexpert.training.R
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Playground() {
-    val dogImage = ImageBitmap.imageResource(id = R.drawable.dog)
     Canvas(modifier = Modifier.fillMaxSize()) {
-        drawImage(dogImage)
+        val path = Path().apply {
+            moveTo(0f, 0f)
+            lineTo(size.width / 2f, size.height / 2f)
+            lineTo(size.width, 0f)
+            close()
+        }
+        drawPath(path, Color.Magenta, style = Stroke(width = 4.dp.toPx()))
     }
 }

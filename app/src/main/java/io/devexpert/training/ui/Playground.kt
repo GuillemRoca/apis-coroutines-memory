c8ab8ca979e5339a5c12f96732f061f19140b816
antoniolg <antonio@devexperto.com>
2024-09-27 12:01:46 +0200

    Canvas: textMeasurer

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -4,13 +4,23 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 
 @Composable
 fun Playground() {
     val textMeasurer = rememberTextMeasurer()
     Canvas(modifier = Modifier.fillMaxSize()) {
-        drawText(textMeasurer, "Hello World")
+        val measuredText = textMeasurer.measure(
+            AnnotatedString("Text with background"),
+            style = TextStyle(fontSize = 18.sp)
+        )
+        drawRect(Color.Magenta, size = measuredText.size.toSize())
+        drawText(measuredText)
     }
 }

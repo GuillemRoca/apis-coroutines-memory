1afff41b71164cced65d66118cf907da76b05dc6
antoniolg <antonio@devexperto.com>
2024-09-27 11:53:30 +0200

    Canvas: drawText

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -4,23 +4,13 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.withTransform
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 
 @Composable
 fun Playground() {
+    val textMeasurer = rememberTextMeasurer()
     Canvas(modifier = Modifier.fillMaxSize()) {
-        withTransform({
-            translate(left = size.width / 5F)
-            rotate(degrees = 45F)
-        }) {
-            drawRect(
-                color = Color.Gray,
-                topLeft = Offset(x = size.width / 3F, y = size.height / 3F),
-                size = Size(size.width / 3F, size.height / 3F)
-            )
-        }
+        drawText(textMeasurer, "Hello World")
     }
 }

883e7888338f639ea14202da5615c387ebc776fc
antoniolg <antonio@devexperto.com>
2024-09-27 11:44:14 +0200

    Canvas: scale

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -6,14 +6,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.scale
 
 @Composable
 fun Playground() {
     Canvas(modifier = Modifier.fillMaxSize()) {
-        drawCircle(
-            color = Color.Blue,
-            radius = size.minDimension / 4,
-            center = Offset(size.width / 4, size.height / 4)
-        )
+        scale(2f) {
+            drawCircle(
+                color = Color.Red,
+                radius = size.minDimension / 4,
+                center = Offset(size.width / 4, size.height / 4)
+            )
+        }
     }
 }

9f6fb13d72bd623ae32a50cd74a83c7766082afb
antoniolg <antonio@devexperto.com>
2024-09-27 11:48:33 +0200

    Canvas: insets

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -4,20 +4,19 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.inset
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Playground() {
     Canvas(modifier = Modifier.fillMaxSize()) {
-        rotate(degrees = 45F) {
-            drawRect(
-                color = Color.Gray,
-                topLeft = Offset(x = size.width / 3F, y = size.height / 3F),
-                size = Size(size.width / 3F, size.height / 3F)
-            )
+        val canvasQuadrantSize = size / 2F
+        inset(
+            horizontal = 20.dp.toPx(),
+            vertical = 20.dp.toPx()
+        ) {
+            drawRect(color = Color.Green, size = canvasQuadrantSize)
         }
     }
 }

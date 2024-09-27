5d9bf629eb33bbd405c8d149a6491c78da5de88c
antoniolg <antonio@devexperto.com>
2024-09-27 12:56:18 +0200

    Canvas: detect drag gestures

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -1,31 +1,38 @@
 package io.devexpert.training.ui
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun Playground() {
-    Text(
-        "Hello Compose!",
+    var position by remember { mutableStateOf(Offset.Zero) }
+
+    Canvas(
         modifier = Modifier
-            .drawWithCache {
-                val brush = Brush.linearGradient(
-                    listOf(Color.Cyan, Color.Green)
-                )
-                onDrawBehind {
-                    drawRoundRect(
-                        brush,
-                        cornerRadius = CornerRadius(10.dp.toPx())
-                    )
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    change.consume()
+                    position += dragAmount
                 }
             }
-            .padding(8.dp)
-    )
+    ) {
+        val center = Offset(size.width / 2, size.height / 2)
+        drawCircle(
+            color = Color.Blue,
+            radius = 50.dp.toPx(),
+            center = center + position
+        )
+    }
 }

8628e9aee370e79dc57c039a28a5f66044368e3e
antoniolg <antonio@devexperto.com>
2024-09-27 11:41:20 +0200

    Canvas: Draw Circle

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -5,19 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Playground() {
     Canvas(modifier = Modifier.fillMaxSize()) {
-        val minSize = size.minDimension / 2f
-        val offsetX = (size.width - minSize) / 2f
-        val offsetY = (size.height - minSize) / 2f
-        drawRect(
-            color = Color.Magenta,
-            topLeft = Offset(offsetX, offsetY),
-            size = Size(minSize, minSize)
+        drawCircle(
+            color = Color.Blue,
+            radius = size.minDimension / 4,
+            center = Offset(size.width / 4, size.height / 4)
         )
     }
 }

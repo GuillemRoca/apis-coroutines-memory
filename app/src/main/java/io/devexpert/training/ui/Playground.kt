53502086acb220249e2afeb36935e295ceba3d94
antoniolg <antonio@devexperto.com>
2024-09-27 18:02:58 +0200

    LazyList: Reacting and controlling scroll position

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -1,144 +1,68 @@
 package io.devexpert.training.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxState
-import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun Playground() {
-    var items by remember { mutableStateOf((0..4).toList()) }
-    var itemCounter by remember { mutableIntStateOf(5) }
     val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val showScrollToTop by remember { derivedStateOf { listState.firstVisibleItemIndex > 5 } }
 
-    Column {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Button(onClick = {
-                val insertIndex = if (items.isEmpty()) 0 else items.indices.random()
-                items = items.toMutableList().apply { add(insertIndex, itemCounter++) }
-            }) {
-                Text("Add Item")
-            }
-            Button(onClick = {
-                if (items.isNotEmpty()) {
-                    val removeIndex = (items.indices).random()
-                    items = items.toMutableList().apply { removeAt(removeIndex) }
-                }
-            }) {
-                Text("Remove Item")
-            }
-        }
+    Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(
             state = listState,
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(8.dp),
             contentPadding = PaddingValues(horizontal = 8.dp)
         ) {
-            items(
-                items,
-                key = { it }
-            ) { item ->
-                val dismissState = rememberSwipeToDismissBoxState(
-                    confirmValueChange = {
-                        items = when (it) {
-                            SwipeToDismissBoxValue.StartToEnd -> {
-                                items.toMutableList().apply { remove(item) }
-                            }
+            items(100) { index ->
+                Card(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Item $index",
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
 
-                            SwipeToDismissBoxValue.EndToStart -> {
-                                items.toMutableList().apply { remove(item) }
-                            }
-
-                            SwipeToDismissBoxValue.Settled -> return@rememberSwipeToDismissBoxState false
-                        }
-                        return@rememberSwipeToDismissBoxState true
-                    },
-                    // positional threshold of 25%
-                    positionalThreshold = { it * .25f }
-                )
-                SwipeToDismissBox(
-                    state = dismissState,
-                    modifier = Modifier.fillMaxWidth().animateItem(),
-                    backgroundContent = { DismissBackground(dismissState) },
-                    content = {
-
-                        Card(
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Text(
-                                text = "Item $item",
-                                modifier = Modifier
-                                    .padding(16.dp)
-                            )
-
-                        }
-                    })
+                }
             }
         }
-    }
-}
-
-@Composable
-fun DismissBackground(dismissState: SwipeToDismissBoxState) {
-    val color = when (dismissState.dismissDirection) {
-        SwipeToDismissBoxValue.StartToEnd -> Color(0xFFFF1744)
-        SwipeToDismissBoxValue.EndToStart -> Color(0xFF1DE9B6)
-        SwipeToDismissBoxValue.Settled -> Color.Transparent
-    }
-
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .clip(MaterialTheme.shapes.medium)
-            .background(color)
-            .padding(12.dp, 8.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Icon(
-            Icons.Default.Delete,
-            contentDescription = "delete"
-        )
-        Spacer(modifier = Modifier)
-        Icon(
-            Icons.Default.Favorite,
-            contentDescription = "favorite"
-        )
+        if (showScrollToTop) {
+            FloatingActionButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.animateScrollToItem(0)
+                    }
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Icon(Icons.Default.KeyboardArrowUp, contentDescription = "Scroll to Top")
+            }
+        }
     }
 }

59c3f8ca733f8a3a6745312f192e8b911b6e6599
antoniolg <antonio@devexperto.com>
2024-10-03 10:55:54 +0200

    Coroutines: job.join() sample

--- app/src/main/java/io/devexpert/training/main.kt
+++ app/src/main/java/io/devexpert/training/main.kt
@@ -1,15 +1,18 @@
 package io.devexpert.training
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    println("Starting to fetch user data")
-    val userData = fetchUserData()
-    println("User data received: $userData")
-}
+    val job = launch {
+        repeat(5) { i ->
+            println("Coroutine is working: $i")
+            delay(500)
+        }
+    }
 
-suspend fun fetchUserData(): String {
-    delay(2000L) // Simulating network call
-    return "User Data"
+    println("Main: Waiting for the coroutine to finish...")
+    job.join()
+    println("Main: Now I can continue!")
 }

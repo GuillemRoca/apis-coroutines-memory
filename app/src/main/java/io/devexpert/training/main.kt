e25cccc40c3cbdff58ffaaceeb6fc6b809743bcc
antoniolg <antonio@devexperto.com>
2024-10-03 12:00:26 +0200

    Coroutines: CoroutineExceptionHandle sample

--- app/src/main/java/io/devexpert/training/main.kt
+++ app/src/main/java/io/devexpert/training/main.kt
@@ -1,30 +1,23 @@
 package io.devexpert.training
 
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.supervisorScope
 
-fun main() = runBlocking {
-    supervisorScope {
-        launch {
-            delay(500)
-            println("First child is completing")
-        }
-
-        launch {
-            delay(1000)
-            println("Second child throws an exception")
-            throw RuntimeException("Oops")
-        }
-
-        launch {
-            delay(2000)
-            println("Third child completes successfully")
-        }
-
-        println("supervisorScope is waiting for children")
+fun main(): Unit = runBlocking {
+    val handler = CoroutineExceptionHandler { _, exception ->
+        println("Caught $exception")
     }
 
-    println("supervisorScope is completed")
+    supervisorScope {
+        launch(handler) {
+            throw RuntimeException("Error in task 1")
+        }
+        launch {
+            delay(100)
+            println("Task 2 completed")
+        }
+    }
 }

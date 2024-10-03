6edf744713987bf0b8286a5f5233a8a34d5bb6b6
antoniolg <antonio@devexperto.com>
2024-10-03 11:52:52 +0200

    Coroutines: supervisorScope() sample

--- app/src/main/java/io/devexpert/training/main.kt
+++ app/src/main/java/io/devexpert/training/main.kt
@@ -1,34 +1,30 @@
 package io.devexpert.training
 
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
 
 fun main() = runBlocking {
-    try {
-        coroutineScope {
-            launch {
-                delay(500)
-                println("First child is completing")
-            }
-
-            launch {
-                delay(1000)
-                println("Second child throws an exception")
-                throw RuntimeException("Oops")
-            }
-
-            launch {
-                delay(2000)
-                println("Third child will not execute this line")
-            }
-
-            println("coroutineScope is waiting for children")
+    supervisorScope {
+        launch {
+            delay(500)
+            println("First child is completing")
         }
-    } catch (e: Exception) {
-        println("Caught: ${e.message}")
+
+        launch {
+            delay(1000)
+            println("Second child throws an exception")
+            throw RuntimeException("Oops")
+        }
+
+        launch {
+            delay(2000)
+            println("Third child completes successfully")
+        }
+
+        println("supervisorScope is waiting for children")
     }
 
-    println("coroutineScope is completed")
+    println("supervisorScope is completed")
 }

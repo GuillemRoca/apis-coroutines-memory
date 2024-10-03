dd51f5fbaaae080c9f251b6fa0aa1ae3a79085dd
antoniolg <antonio@devexperto.com>
2024-10-03 10:26:09 +0200

    Coroutines: suspend Example

--- app/src/main/java/io/devexpert/training/main.kt
+++ app/src/main/java/io/devexpert/training/main.kt
@@ -1,13 +1,15 @@
 package io.devexpert.training
 
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    launch {
-        delay(1000L)
-        println("World!")
-    }
-    println("Hello,")
+    println("Starting to fetch user data")
+    val userData = fetchUserData()
+    println("User data received: $userData")
+}
+
+suspend fun fetchUserData(): String {
+    delay(2000L) // Simulating network call
+    return "User Data"
 }

46cec55be5e03d6f79886f7a740e77b59c7941fb
antoniolg <antonio@devexperto.com>
2024-10-04 11:19:26 +0200

    Coroutines: Flow combine exercise

--- app/src/main/java/io/devexpert/training/main.kt
+++ app/src/main/java/io/devexpert/training/main.kt
@@ -1,25 +1,32 @@
 package io.devexpert.training
 
-import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 
+// Create two flows:
+// 1. Emits numbers 1 to 5 with 100ms delay
+// 2. Emits letters A to E with 150ms delay
+// Combine these flows
+// Collect and print the combined results
+
 fun main() = runBlocking {
-    flow {
-        for (i in 1..100) {
-            delay(20)
+    val flow1 = flow {
+        for (i in 1..5) {
+            delay(100)
             emit(i)
-            println("Emitted $i")
         }
     }
-        //.buffer(10)
-        //.buffer(10, BufferOverflow.DROP_OLDEST)
-        //.conflate()
-        //.collectLatest { value ->
+    val flow2 = flow {
+        for (char in 'A'..'E') {
+            delay(150)
+            emit(char.toString())
+        }
+    }
+
+    flow1.combine(flow2) { num, char -> "$num$char" }
         .collect { value ->
-            delay(100)
-            println("Collected $value")
+            println(value)
         }
 }

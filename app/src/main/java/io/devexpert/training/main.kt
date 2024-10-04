b480e4250c5cc812ba8cb07189da5bae32a6caef
antoniolg <antonio@devexperto.com>
2024-10-04 11:44:30 +0200

    Coroutines: Flow backpressure example

--- app/src/main/java/io/devexpert/training/main.kt
+++ app/src/main/java/io/devexpert/training/main.kt
@@ -1,28 +1,25 @@
 package io.devexpert.training
 
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 
-// Create a flow that emits numbers from 1 to 5
-// with a 100ms delay between each emission
-// Double each number
-// Filter out odd numbers
-// Collect and print the results
-
 fun main() = runBlocking {
-    val flow = flow {
-        for (i in 1..5) {
-            delay(100)
+    flow {
+        for (i in 1..100) {
+            delay(20)
             emit(i)
+            println("Emitted $i")
         }
     }
-        .map { it * 2 }
-        .filter { it % 2 == 0 }
-
-    flow.collect { value ->
-        println(value)
-    }
+        //.buffer(10)
+        //.buffer(10, BufferOverflow.DROP_OLDEST)
+        //.conflate()
+        //.collectLatest { value ->
+        .collect { value ->
+            delay(100)
+            println("Collected $value")
+        }
 }

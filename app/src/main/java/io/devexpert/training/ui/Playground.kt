e3440550f3b37d6b5124a98225398bffc3448339
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2024-10-14 09:48:28 +0200

    Scale example

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -1,14 +1,14 @@
 package io.devexpert.training.ui
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.scale
+import kotlin.math.min
 
 @Composable
 fun Playground() {
@@ -16,10 +16,17 @@ fun Playground() {
         val minSize = size.minDimension / 2f
         val offsetX = (size.width - minSize) / 2f
         val offsetY = (size.height - minSize) / 2f
-        drawRect(
-            color = Color.Magenta,
-            topLeft = Offset(offsetX, offsetY),
-            size = Size(minSize,minSize)
+        scale(1.5f) {
+            drawRect(
+                color = Color.Red,
+                topLeft = Offset(offsetX, offsetY),
+                size = Size(minSize,minSize)
+            )
+        }
+        drawCircle(
+            color = Color.Yellow,
+            center = center,
+            radius = minSize / 2f
         )
     }
 }

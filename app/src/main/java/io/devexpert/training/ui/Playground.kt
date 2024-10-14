84e438f78f78050e8ca07dfcb5a1d23541d39292
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2024-10-14 09:28:06 +0200

    More drawable examples

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 
 @Composable
@@ -14,5 +15,9 @@ fun Playground() {
     Spacer(
         modifier = Modifier
             .fillMaxSize()
-            .drawBehind { drawRect(color = Color.Magenta, size = size / 2f) })
+            .drawBehind {
+                drawRect(color = Color.Magenta, size = size / 2f, topLeft = Offset(200f, 200f))
+                drawCircle(color = Color.Blue, radius = 100f, center = Offset(300f, 300f))
+            }
+    )
 }

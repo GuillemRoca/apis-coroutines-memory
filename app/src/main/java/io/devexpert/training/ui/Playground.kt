eef798219ea86b031852cd2316ec8bf5721648bc
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2024-10-16 09:29:11 +0200

    Animate as State example

--- app/src/main/java/io/devexpert/training/ui/Playground.kt
+++ app/src/main/java/io/devexpert/training/ui/Playground.kt
@@ -1,6 +1,7 @@
 package io.devexpert.training.ui
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -9,14 +10,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -32,25 +37,21 @@ import kotlin.math.min
 
 @Composable
 fun Playground() {
-    var expanded by remember { mutableStateOf(false) }
-    Column(modifier = Modifier.padding(16.dp)) {
-        Button(onClick = { expanded = !expanded }) {
-            Text("Toggle Content")
+    var scale by remember { mutableFloatStateOf(1f) }
+    val animatedScale by animateFloatAsState(targetValue = scale, label = "scale")
+
+    Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick = { scale = if (scale == 1f) 1.5f else 1f }) {
+            Text("Toggle Scale")
         }
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(32.dp))
         Box(
             modifier = Modifier
-                .background(Color.LightGray)
-                .animateContentSize()
+                .size(100.dp)
+                .scale(animatedScale)
+                .background(Color.Green)
         ) {
-            Text(
-                text = if (expanded) {
-                    "This is expanded content with more text."
-                } else {
-                    "Click to expand."
-                },
-                modifier = Modifier.padding(16.dp)
-            )
+            Text("Scaling", modifier = Modifier.align(Alignment.Center))
         }
     }
 }
